defa19668d144a3ea890b9b0ae80521b5bfe5e08
Sandra S. Templeton <10134142+idivait@users.noreply.github.com>
2018-08-21 17:02:44 -0500

    Stash changes

--- app/validators/user.js
+++ app/validators/user.js
@@ -28,11 +28,7 @@ export default PasswordValidator.create({
 
         if (this.isActive(model)) {
             // Change length to 600. idiva
-<<<<<<< HEAD
             if (!validator.isLength(bio || '', 0, 600)) {
-=======
-            if (!validator.isLength(bio, 0, 600)) {
->>>>>>> Mod admin client to allow 600 length.
                 model.get('errors').add('bio', 'Bio is too long');
                 this.invalidate();
             }
